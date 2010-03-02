cef9c2897fb5b6aa9fa469c2aeee2490ea3e9fd5
scifisamurai <scifi@Labyrinth.hsd1.fl.comcast.net>
2010-03-02 18:08:44 -0500

    Moved code from tests file to main code file

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
-#!/usr/bin/python
+#!/usr/bin/env python
+import tests #unit tests
 
+def to_roman(number):
+    q, r = divmod(number, 5)
+    if number == 8:
+        return "VIII"
+    if number == 5:
+        return "V"
+    if number == 4:
+        return "IV"
+    if number < 4:
+        return number * "I"
 
+if __name__ == "__main__":
+	x=tests.TestProgram
+	x(module='tests') #run our tests
--- tests.py
+++ tests.py
@@ -1,39 +1,31 @@
-from unittest import TestCase, main
+#from unittest import TestCase, main
+from unittest import TestCase, TestProgram
+#from unittest import TestCase #testing framework
+import main #code being testing
 
-def to_roman(number):
-    q, r = divmod(number, 5)
-    if number == 8:
-        return "VIII"
-    if number == 5:
-        return "V"
-    if number == 4:
-        return "IV"
-    if number < 4:
-        return number * "I"
+class TestConvertToRoman(TestCase):
+	def test_convert_1_to_roman(self):
+		self.assertEquals("I", main.to_roman(1))
 
+	def test_convert_2_to_roman(self):
+		self.assertEquals("II",main.to_roman(2))
+
+	def test_convert_3_to_roman(self):
+		self.assertEquals("III",main.to_roman(3))
+
+	def test_convert_4_to_roman(self):
+		self.assertEquals("IV",main.to_roman(4))
+
+	def test_convert_5_to_roman(self):
+		self.assertEquals("V",main.to_roman(5)) 
+
+	def test_convert_8_to_roman(self):
+		self.assertEquals("VIII",main.to_roman(8))       
+
+#def doIt(self):
     
-
-class TestConverToRoman(TestCase):
-    def test_convert_1_to_roman(self):
-        self.assertEquals("I", to_roman(1))
-
-    def test_convert_2_to_roman(self):
-        self.assertEquals("II", to_roman(2))
-
-    def test_convert_3_to_roman(self):
-        self.assertEquals("III", to_roman(3))
-
-    def test_convert_4_to_roman(self):
-        self.assertEquals("IV", to_roman(4))
-
-    def test_convert_5_to_roman(self):
-        self.assertEquals("V", to_roman(5)) 
-
-    def test_convert_8_to_roman(self):
-        self.assertEquals("VIII", to_roman(8))       
-    
-
 if __name__ == "__main__":
-    main()
-
-
+	#doIt
+	c=TestProgram
+	c()
+#	main()
